7af33e8b4a81c24e4374f89f3d81546f1d42c286
LuisKaczam <luisfelipekaczam@gmail.com>
2022-07-04 12:58:49 -0300

    Inclusão do código HTML

---
+++ ProjetoWeb/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      adelia: ["ADELIA", "cursive"],
+    }
+  },
+  plugins: [],
+}
